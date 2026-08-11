f383cddf7388bcce9e3fe76d06356c986f042956
Antonyx78 <antonygabrielsm@gmail.com>
2026-08-11 11:20:48 -0300

    adicionando eventos

---
+++ src/data/ingresso.js
@@ -0,0 +1,52 @@
+// src/data/ingresso.js
+
+export const eventos = [
+  {
+    id: 1,
+    nome: "Rock in Rio 2026",
+    local: "Parque Olímpico - Rio de Janeiro, RJ",
+    capacidade: 100000,
+    preco: 795.00,
+    imagem: "https://betimelapse.com.br/_next/image?url=https%3A%2F%2Fmedia.betimelapse.com.br%2Fuploads%2Feventos%2FRock-in-Rio-2026-Buyticket-Capa-Timelapse.jpg&w=1920&q=75"
+  },
+  {
+    id: 2,
+    nome: "BTS - ARIRANG World Tour",
+    local: "MorumBIS - São Paulo, SP",
+    capacidade: 66000,
+    preco: 450.00,
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq84Zh3mSYQ1tMtRdwg3M_6OECbgsQIk5kESvvcwLX7ICjR3rR0Fb2WdY&s=10"
+  },
+  {
+    id: 3,
+    nome: "Ed Sheeran - Loop Tour",
+    local: "Nubank Parque - São Paulo, SP",
+    capacidade: 45000,
+    preco: 380.00,
+    imagem: "https://www.eventim.com.br/campaign/fileadmin/fm_br/campaigns/2026/eds/ED_Nubank.jpg"
+  },
+  {
+    id: 4,
+    nome: "Iron Maiden - Run For Your Lives",
+    local: "Arena da Baixada - Curitiba, PR",
+    capacidade: 42000,
+    preco: 320.00,
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJqU0VII_gLj8U_AqpuSg3HfcBj6nLbSPRB4SpoL08kw&s"
+  },
+  {
+    id: 5,
+    nome: "Primavera Sound São Paulo 2026",
+    local: "Autódromo de Interlagos - São Paulo, SP",
+    capacidade: 80000,
+    preco: 425.00,
+    imagem: "https://kraken.ingresse.com/event/posters/93986/large/1785522828.7792883.jpg"
+  },
+  {
+    id: 6,
+    nome: "Rosalía - LUX World Tour",
+    local: "Farmasi Arena - Rio de Janeiro, RJ",
+    capacidade: 18000,
+    preco: 290.00,
+    imagem: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxibv4b7eTkM6_wOnVxLF8h4VH2uvBeO4brcllU-jQP-nRdYuBdOKo4EU&s=10"
+  }
+];
